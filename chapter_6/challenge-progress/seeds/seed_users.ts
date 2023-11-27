46f4da3832c70d4fd5f5ece2ea2fd22914e8df7b
ahmadseloabadi <ahmadseloabadi@gmail.com>
2023-11-27 23:19:19 +0700

    challenge progres

---
+++ chapter_6/challenge-progress/seeds/seed_users.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+import bcrypt from "bcrypt";
+
+export async function seed(knex: Knex): Promise<void> {
+  const SALT = bcrypt.genSaltSync(10);
+  const password = bcrypt.hashSync("superadmin", SALT);
+
+  // Deletes ALL existing entries
+  await knex("users").del();
+
+  // Inserts seed entries
+  await knex("users").insert([
+    {
+      email: "superadmin@superadmin.com",
+      password,
+      role: "superadmin",
+    },
+  ]);
+}
