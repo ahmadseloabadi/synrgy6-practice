dd4a9e634ea593bb764e483423342680b1ceee6d
ahmadseloabadi <ahmadseloabadi@gmail.com>
2023-12-02 17:58:59 +0700

    redesain ui login

--- chapter_7/react-app/src/pages/Login.tsx
+++ chapter_7/react-app/src/pages/Login.tsx
@@ -9,60 +9,57 @@ export default function Login() {
   const tweets_api_base_url = "http://localhost:8000";
 
   return (
-    <div className="flex items-center  justify-center px-6 py-8 mx-auto md:h-screen  lg:py-0">
-      <div className="form p-7 dark:bg-gray-900 rounded-xl ">
-        <h1 className="flex items-center justify-center mb-6 text-2xl font-semibold dark:text-white">
+    <div className="flex items-center  justify-end  mx-auto md:h-screen  lg:py-0">
+      <img src="../src/assets/bg-login.png" className="w-2/3" />
+      <div className="form p-7s rounded-xl w-1/3">
+        <h1 className="flex justify-center mb-6 text-2xl font-semibold dark:text-white">
           Login
         </h1>
 
-        <form className=" w-full max-w-sm ">
-          <div className="mb-3 flex flex-row">
-            <div className="md:w-1/3">
-              <label
-                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                htmlFor="inline-full-name"
-              >
-                Email :
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                id="inline-full-name"
-                type="email"
-                value={email}
-                onChange={({ target }) => {
-                  setEmail(target.value);
-                }}
-                placeholder="Masukkan email"
-              />
-            </div>
+        <form className=" w-full justify-center  grid gap-y-4">
+          <div className="logo w-[100px] h-9 bg-gray-300"></div>
+          <h1 className="text-2xl font-bold">Welcome, Admin BCR</h1>
+          <div className="grid w-full">
+            <label
+              className=" font-light text-sm  mb-1 md:mb-0 pr-4"
+              htmlFor="email"
+            >
+              Email :
+            </label>
+            <input
+              className=" appearance-none border-2 border-gray-200 py-2 px-3 rounded w-[370px] leading-tight focus:outline-none focus:bg-white  font-light text-sm "
+              id="email"
+              type="email"
+              value={email}
+              onChange={({ target }) => {
+                setEmail(target.value);
+              }}
+              placeholder="Masukkan email"
+            />
           </div>
-          <div className="mb-3 flex flex-row">
-            <div className="md:w-1/3">
-              <label
-                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                htmlFor="inline-full-name"
-              >
-                Password :
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                id="inline-full-name"
-                type="password"
-                value={password}
-                onChange={({ target }) => {
-                  setPassword(target.value);
-                }}
-                placeholder="Masukkan password"
-              />
-            </div>
+
+          <div className="grid justify-center w-full">
+            <label
+              className="font-light text-sm   mb-1 md:mb-0 pr-4"
+              htmlFor="password"
+            >
+              Password :
+            </label>
+            <input
+              className=" appearance-none border-2 border-gray-200 py-2 px-3 rounded w-[370px] leading-tight focus:outline-none focus:bg-white  font-light text-sm "
+              id="password"
+              type="password"
+              value={password}
+              onChange={({ target }) => {
+                setPassword(target.value);
+              }}
+              placeholder="Masukkan password"
+            />
           </div>
+
           <div className="flex justify-center">
             <button
-              className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded "
+              className="shadow bg-blue-900 hover:bg-blue-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-3 rounded w-[370px] "
               typeof="button"
               onClick={async (e) => {
                 e.preventDefault();
@@ -96,7 +93,7 @@ export default function Login() {
                 navigate("/");
               }}
             >
-              Login
+              Sign In
             </button>
           </div>
         </form>
